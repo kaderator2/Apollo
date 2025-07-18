b283c5068efc2d2b94bd7573e780452b86c95db9
chadePC <kadehdean@gmail.com>
2025-07-18 12:13:24 -0700

    update display_device to make linux settings manager

--- src/display_device.cpp
+++ src/display_device.cpp
@@ -25,6 +25,8 @@
   #include <display_device/windows/settings_manager.h>
   #include <display_device/windows/win_api_layer.h>
   #include <display_device/windows/win_display_device.h>
+#elif __linux__
+  #include "platform/linux/LinuxSettingsManager.h"  // Include our new class
 #endif
 
 namespace display_device {
@@ -624,6 +626,9 @@ namespace display_device {
           .m_hdr_blank_delay = video_config.dd.wa.hdr_toggle_delay != std::chrono::milliseconds::zero() ? std::make_optional(video_config.dd.wa.hdr_toggle_delay) : std::nullopt
         }
       );
+#elif __linux__
+      // new Linux implementation
+      return std::make_unique<LinuxSettingsManager>();
 #else
       return nullptr;
 #endif
@@ -764,12 +769,14 @@ namespace display_device {
   }
 
   std::string map_display_name(const std::string &display_name) {
-    std::lock_guard lock { DD_DATA.mutex };
+    std::lock_guard lock {DD_DATA.mutex};
     if (!DD_DATA.sm_instance) {
       return {};
     }
 
-    const auto available_devices { DD_DATA.sm_instance->execute([](auto &settings_iface) { return settings_iface.enumAvailableDevices(); }) };
+    const auto available_devices {DD_DATA.sm_instance->execute([](auto &settings_iface) {
+      return settings_iface.enumAvailableDevices();
+    })};
 
     for (auto &i : available_devices) {
       if (i.m_display_name == display_name) {
@@ -781,8 +788,8 @@ namespace display_device {
   }
 
   void configure_display(const config::video_t &video_config, const rtsp_stream::launch_session_t &session) {
-    const auto result { parse_configuration(video_config, session) };
-    if (const auto *parsed_config { std::get_if<SingleDisplayConfiguration>(&result) }; parsed_config) {
+    const auto result {parse_configuration(video_config, session)};
+    if (const auto *parsed_config {std::get_if<SingleDisplayConfiguration>(&result)}; parsed_config) {
       configure_display(*parsed_config);
       return;
     }
